3756fbd40fd3a89e7a7720a8b43464f68d35714d
Nico Rithner <nicorithner@gmail.com>
2020-09-19 00:41:51 -0600

    Create '#create' action for discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,5 +1,18 @@
 class Merchant::DiscountsController < Merchant::BaseController
   def new
-    
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.create(discount_params)
+    discount.save
+    redirect_to "/merchant/discounts"
+    flash[:success] = "Discount #{discount.name} created!"
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :discount_percentage, :minimum_quantity)
   end
 end
