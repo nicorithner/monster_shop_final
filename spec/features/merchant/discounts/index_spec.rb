7d3619cedf80b751e61a8da752500ad1d97f45d5
Nico Rithner <nicorithner@gmail.com>
2020-09-19 06:58:04 -0600

    Create feature test it has an index page

---
+++ spec/features/merchant/discounts/index_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "New Discount Page" do
+  before :each do
+    @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+    
+    @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+    
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+
+    @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+    @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+    @hippo = @merchant_1.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
+
+    @order_1 = @m_user.orders.create!(status: "pending")
+    @order_2 = @m_user.orders.create!(status: "pending")
+    @order_3 = @m_user.orders.create!(status: "pending")
+
+    @order_item_1 = @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: false)
+    @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
+    @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
+    @order_item_4 = @order_3.order_items.create!(item: @giant, price: @giant.price, quantity: 2, fulfilled: false)
+
+    @discount_1 = @merchant_1.discounts.create(name: "5% off 5", discount_percentage: 5, minimum_quantity: 5)
+    @discount_2 = @merchant_1.discounts.create(name: "10% off 10", discount_percentage: 10, minimum_quantity: 10)
+  end
+
+  describe "Discounts are display in the merchant discounts index page" do
+
+    it "Merchant discounts are listed in the index page" do
+      visit '/merchant/discounts'
+      save_and_open_page
+      expect(page).to have_content(@discount_1.name)
+      expect(page).to have_content(@discount_1.discount_percentage)
+      expect(page).to have_content(@discount_1.minimum_quantity)
+    end
+  end
+end
