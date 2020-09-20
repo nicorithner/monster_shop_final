10937f6450003045acaac4430cacbf01fa19f4fc
Nico Rithner <nicorithner@gmail.com>
2020-09-20 11:35:18 -0600

    Create methods to obtain subtotal considering discounts. ISSUE with #match_by_discount_criteria when the return is nil.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -37,10 +37,33 @@ class Cart
   end
 
   def subtotal_of(item_id)
-    @contents[item_id.to_s] * Item.find(item_id).price
+    price = if discount_value(item_id) != 0
+      Item.find(item_id).price * discount_value(item_id)
+    else
+      Item.find(item_id).price
+    end
+    @contents[item_id.to_s] * price
   end
 
   def limit_reached?(item_id)
     count_of(item_id) == Item.find(item_id).inventory
+  end
+
+  def check_for_item_discounts(item_id)
+    Item.find(item_id).merchant.discounts
+  end
+
+  def match_by_discount_criteria(item_id)
+    discounts = check_for_item_discounts(item_id)
+    total = @contents[item_id.to_s]
+    if total >= discounts.select(:minimum_quantity).find_by(minimum_quantity: total).minimum_quantity
+      discounts.find_by(minimum_quantity: total)
+    end
+  end
+
+  def discount_value(item_id)
+    if match_by_discount_criteria(item_id)
+      match_by_discount_criteria(item_id).discount_percentage
+    end
   end
 end
