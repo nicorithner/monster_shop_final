ed643cc33977affba1d698d534990e33427c897f
Nico Rithner <nicorithner@gmail.com>
2020-09-21 16:29:06 -0600

    Add addresses relationship to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   belongs_to :merchant, optional: true
   has_many :orders
+  has_many :adresses
 
   validates_presence_of :name,
                         :address,
